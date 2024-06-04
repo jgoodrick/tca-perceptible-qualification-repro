05b93b48d4544b4701df686c5e25503ffcc131af
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2024-06-04 12:33:49 -0500

    Initial Commit

---
+++ TCA-Perceptible-Qualification-Bug/TCA_Perceptible_Qualification_BugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Perceptible_Qualification_BugApp.swift
+//  TCA-Perceptible-Qualification-Bug
+//
+//  Created by Joseph Goodrick on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Perceptible_Qualification_BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
